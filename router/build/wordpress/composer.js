0fb3edd97932e087cf53902158f134dfd34e30a5
khoibn <buinguyenkhoi96@gmail.com>
2018-06-07 14:25:22 +0700

    composer

--- router/build/wordpress/composer.js
+++ router/build/wordpress/composer.js
@@ -4,6 +4,7 @@ import { asyncMiddleware } from "../../../midlewares/AsyncMiddleware";
 import { Exception } from "../../../app/Exceptions/Exception";
 import * as _ from "lodash";
 import AuthMiddleware from "../../../midlewares/AuthMiddleware";
+import fs from "fs";
 
 let router = express.Router();
 
@@ -12,7 +13,6 @@ router.post("/", asyncMiddleware(runComposer));
 async function runComposer(req, res) {
   try {
     let website = req.body.website;
-    let select = req.body.select;
     if (!website) {
       throw new Error("website not empty");
     }
@@ -21,27 +21,29 @@ async function runComposer(req, res) {
     let query = new WordpressQuery();
     query.moveDir(website);
     let file = await query.findFile("composer.json");
-    _.forEach(file, item => {
-      if (item.indexOf("themes") > -1) {
-        path = item.slice(1, -13);
-      }
-    });
+    // _.forEach(file, item => {
+    //   if (item.indexOf("themes") > -1) {
+    //     path = item.slice(1, -13);
+    //   }
+    // });
 
-    query.moveDir(website, path);
+    console.log(file);
+    // query.moveDir(website, path);
 
-    let composer = await query.findFile("composer.json");
-    let result;
-    if (composer.success) {
-      await query.runComposerWordpress("rm -rf composer.lock");
-      await query.runComposerWordpress("composer install");
-      await query.runComposerWordpress("composer dump-autoload -o");
-      result = result = { success: true };
-    } else {
-      result = { success: true };
-    }
+    // let composer = await query.findFile("composer.json");
+    // console.log(fs.existsSync('composer.json'));
+    // let result;
+    // if (fs.existsSync('composer.json')) {
+    //   await query.runComposerWordpress("rm -rf composer.lock");
+    //   await query.runComposerWordpress("composer install");
+    //   await query.runComposerWordpress("composer dump-autoload -o");
+    //   result = result = { success: true };
+    // } else {
+    //   result = { success: true };
+    // }
 
     // await query.runComposerWordpress("chown -R jenkins:userweb vendor");
-    res.json({ data: result });
+    res.json({ data: 'result' });
   } catch (e) {
     if (e.error_code) {
       throw new Exception(e.message, e.error_code);
