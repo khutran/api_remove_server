25600507cc7f7e21c760036d083c062587c450d5
khutx <khutx@vicoders.com>
2018-08-09 09:29:40 +0700

    update

--- router/nodejs/database.js
+++ router/nodejs/database.js
@@ -71,7 +71,7 @@ async function deleteDb(req, res) {
     let query = new NodejsQuery();
     query.moveDir(website);
     let config = await query.readEnv(".env");
-    console.log(config["DB_USER"], config["DB_NAME"]);
+    console.log(config);
     // let q = await query.deleteDatabase(config["DB_USER"], config["DB_NAME"]);
 
     res.json({ data: 'q' });
